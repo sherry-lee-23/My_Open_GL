fcd559b1759dae28ce1ef2e07f9fea9c8b851ba1
zhangqiming <zhangqiming061218@outlook.com>
2026-05-09 16:21:59 +0800

    shader & textures

--- Open_GL/Getting started/Textures.cpp
+++ Open_GL/Getting started/Textures.cpp
@@ -1,6 +1,8 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include"stb_image.h"
 #include"shader.h"
 
 #include<iostream>
@@ -43,31 +45,66 @@ int main() {
     }
 
     Shader ourShader(
-        "C:\\Users\\Lenovo\\source\\repos\\Open_GL\\Open_GL\\Getting started\\ourShader.vs",
-        "C:\\Users\\Lenovo\\source\\repos\\Open_GL\\Open_GL\\Getting started\\ourShader.fs"
+        "C:\\Users\\Lenovo\\source\\repos\\Open_GL\\Open_GL\\Getting started\\shader_for_textures.vs",
+        "C:\\Users\\Lenovo\\source\\repos\\Open_GL\\Open_GL\\Getting started\\shader_for_textures.fs"
     );
-        
+
     float verrtices[] = {
-        -0.5f,0.5f,0.0f, 1.0f,0.0f,0.0f,
-        0.5f,0.5f,0.0f, 0.0f,1.0f,0.0f,
-        0.0f,-0.5f,0.0f, 0.0f,0.0f,1.0f  
+                                        //Œ∆¿Ì
+        0.5f, 0.5f, 0.0f,   1.0f,0.0f,0.0f, 1.0f,1.0f,
+        0.5f,-0.5f, 0.0f,   0.0f,1.0f,0.0f, 1.0f,0.0f,
+        -0.5f,-0.5f,0.0f,   0.0f,0.0f,1.0f, 0.0f,0.0f,
+        -0.5f, 0.5f,0.0f,   1.0f,1.0f,0.0f, 0.0f,1.0f
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {
+        0,1,3,
+        1,2,3
+    };
+
+    unsigned int VBO, VAO,EBO;
     glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+    glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verrtices), verrtices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+
+    unsigned char* data = stbi_load("resources/awesomeface.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -75,9 +112,11 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindTexture(GL_TEXTURE_2D, texture);
+
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -86,8 +125,9 @@ int main() {
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
-	glfwTerminate();
-	return 0;
+
+    glfwTerminate();
+    return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
