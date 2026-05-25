2813ac900cf3928cb725a70aa7f43aa1591cdc0f
zhangqiming <zhangqiming061218@outlook.com>
2026-05-25 10:25:32 +0800

    材质，环境光照 漫反射 镜面反射

--- Open_GL/Lighting/shader.h
+++ Open_GL/Lighting/shader.h
@@ -82,7 +82,7 @@ public:
 	}
 
 	void setFloat(const std::string& name, float value) const {
-		glUniform1d(glGetUniformLocation(ID, name.c_str()), value);
+		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 	}
 
 	void setMat4(const std::string& name, const glm::mat4& mat) const {
